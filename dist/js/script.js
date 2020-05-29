7b91f9604025338c72e6d22c72c9540288fbd5fe
nadya <nadya.wyn@ya.ru>
2020-05-29 15:58:47 +0500

    Added Carousel 2

--- dist/js/script.js
+++ dist/js/script.js
@@ -20,5 +20,26 @@ $(document).ready(function () {
 		]
 	});
 
+	$('.carousel2__content').slick({
+		autoplay: false,
+		infinite: true,
+		dots: true,
+		arrows: false,
+		speed: 1800,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		adaptiveHeight: false,
+		autoplaySpeed: 2000,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					dots: false,
+					arrows: false
+				}
+			}
+		]
+	});
+
 
 });  
